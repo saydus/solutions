51548f543cbd41aac49dbf8b6d2f0594bad08bd5
aadarshjha <jha.aadarsh@gmail.com>
2021-06-24 10:17:20 -0500

    70

---
+++ leetcode/70-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> steps(n); 
+        
+        if (n == 1) {
+            return 1; 
+        } else if (n == 2) {
+            return 2; 
+        }
+        
+        steps[0] = 1; 
+        steps[1] = 2; 
+        
+        for (int i = 2; i < n; ++i) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+        }
+        return steps[n - 1];
+    }
+};
